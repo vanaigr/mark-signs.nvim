17c6003d23ba5bac2356bde2f7f4903e58b019e5
vanaigr <vanaigranov@gmail.com>
2024-04-24 22:20:55 -0500

    add mark options

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 A simple plugin for displaying marks in the sign column.
 
-![image](https://github.com/vanaigr/mark-signs.nvim/assets/65824523/96c8d64e-67b7-4908-a8b7-3f9d7e96fb6c)
+![image](https://github.com/vanaigr/mark-signs.nvim/assets/65824523/b6ede486-0c3c-433a-aab2-e7a940cf2ecf)
 
 # Usage
 
 ```lua
-vim.opt.signcolumn = 'yes:1' -- optional
+vim.opt.signcolumn = 'yes:1' -- optional but recommended
 
-local update_marks = require('mark-signs').update_marks
+local ms = require('mark-signs')
+
+-- hidden, priority, sign_hl, number_hl, line_hl, cursorline_hl,
+ms.mark_number_options ({ priority = 10, sign_hl = 'Comment' })
+ms.mark_builtin_options({ priority = 11, sign_hl = 'Comment' })
+ms.mark_lower_options  ({ priority = 12, sign_hl = 'Normal', number_hl = 'CursorLineNr' })
+ms.mark_upper_options  ({ priority = 13, sign_hl = 'Normal', number_hl = 'CursorLineNr' })
+ms.mark_options('.', { hidden = true })
 
 if MarksTimer then MarksTimer:stop() end
 MarksTimer = vim.uv.new_timer()
-MarksTimer:start(0, 400, vim.schedule_wrap(update_marks))
+MarksTimer:start(0, 400, vim.schedule_wrap(ms.update_marks))
 ```
 
 # Acknowledgements
--- lua/mark-signs.lua
+++ lua/mark-signs.lua
@@ -3,15 +3,70 @@ local vim = vim
 local M = {}
 
 M.ns = vim.api.nvim_create_namespace('MarkSigns')
+M.__marks = {}
+
+function M.clear(buf_id)
+    vim.api.nvim_buf_clear_namespace(0, M.ns, 0, -1)
+end
 
 -- 32 is space (not a mark)
-function M.mark2id(name) return name:byte(1) - 32 end
+function M.__mark2id(name) return name:byte(1) - 32 end
+
+function M.__compile_options(opts)
+    local result = {
+        opts.hidden,
+        opts.priority,
+        opts.sign_hl,
+        opts.number_hl,
+        opts.line_hl,
+        opts.cursorline_hl,
+    }
+    return result
+end
+
+function M.mark_lower_options(opts)
+    opts = M.__compile_options(opts)
+    for i = M.__mark2id('a'), M.__mark2id('z') do
+        M.__marks[i] = opts
+    end
+end
+
+function M.mark_upper_options(opts)
+    opts = M.__compile_options(opts)
+    for i = M.__mark2id('A'), M.__mark2id('Z') do
+        M.__marks[i] = opts
+    end
+end
+
+function M.mark_number_options(opts)
+    opts = M.__compile_options(opts)
+    for i = M.__mark2id('0'), M.__mark2id('9') do
+        M.__marks[i] = opts
+    end
+end
+
+-- hopefully all of them (` and ' are same)
+M.__builtin_mark_names = vim.split([==[<>[]"'.^]==], '')
+function M.mark_builtin_options(opts)
+    opts = M.__compile_options(opts)
+    for _, name in ipairs(M.__builtin_mark_names) do
+        M.__marks[M.__mark2id(name)] = opts
+    end
+end
+
+function M.mark_options(name, opts)
+    M.__marks[M.__mark2id(name)] = M.__compile_options(opts)
+end
 
 function M.update_mark(mark, extmarks_by_id)
     local create, update
 
     local mname = mark.mark:sub(2)
-    local id = M.mark2id(mname)
+    local id = M.__mark2id(mname)
+
+    local mark_opts = M.__marks[id] or {}
+    if mark_opts[1] then return end
+
     local mpos = mark.pos
     local mline = mpos[2] - 1
 
@@ -30,7 +85,14 @@ function M.update_mark(mark, extmarks_by_id)
         local opts = {
             strict = false, -- for some reason '> returns column > line byte count
             id = id, sign_text = mname,
+
+            priority = mark_opts[2],
+            sign_hl_group = mark_opts[3],
+            number_hl_group = mark_opts[4],
+            line_hl_group = mark_opts[5],
+            cursorline_hl_group = mark_opts[6],
         }
+
         vim.api.nvim_buf_set_extmark(0, M.ns, mline, mcol, opts)
     end
 
